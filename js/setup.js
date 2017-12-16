7a6c1c41b094f5c90deafe68ece7d16d6b62a85d
Denis Bozhko <Mefisto-92@mail.ru>
2017-12-16 17:54:36 +0300

    Version 1.1

--- js/setup.js
+++ js/setup.js
@@ -21,11 +21,7 @@
 
   var updateWizards = function () {
     window.render(wizards.slice().sort(function (left, right) {
-      var rankDiff = getRank(right) - getRank(left);
-      if (rankDiff === 0) {
-        rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
-      }
-      return rankDiff;
+      return getRank(right) - getRank(left);
     }));
   };
 
--- js/wizard.js
+++ js/wizard.js
@@ -38,18 +38,18 @@
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  var wizardCoat = document.querySelector('.wizard-coat');
-  var inputWizardCoat = document.querySelector('.setup-wizard-appearance input:first-of-type');
-  var wizardEyes = document.querySelector('.wizard-eyes');
-  var inputWizardEyes = document.querySelector('.setup-wizard-appearance input:last-of-type');
-  var wizardFireball = document.querySelector('.setup-fireball-wrap');
-  var inputWizardFireball = document.querySelector('.setup-fireball-wrap input');
+  var wizardCoatElement = document.querySelector('.wizard-coat');
+  var inputWizardCoatElement = document.querySelector('.setup-wizard-appearance input:first-of-type');
+  var wizardEyesElement = document.querySelector('.wizard-eyes');
+  var inputWizardEyesElement = document.querySelector('.setup-wizard-appearance input:last-of-type');
+  var wizardFireballElement = document.querySelector('.setup-fireball-wrap');
+  var inputWizardFireballElement = document.querySelector('.setup-fireball-wrap input');
 
   var fillElement = function (element, colors, input, evt) {
     var randomColor = colors[getRandomInt(0, colors.length - 1)];
     element.style.fill = randomColor;
     input.value = randomColor;
-    if (evt.target === wizardCoat) {
+    if (evt.target === wizardCoatElement) {
       window.wizard.onCoatChange(randomColor);
     } else {
       window.wizard.onEyesChange(randomColor);
@@ -62,7 +62,7 @@
     input.value = randomColor;
   };
 
-  window.colorize.colorizeElement(wizardCoat, WIZARD_COAT_COLORS, fillElement, inputWizardCoat);
-  window.colorize.colorizeElement(wizardEyes, WIZARD_EYES_COLORS, fillElement, inputWizardEyes);
-  window.colorize.colorizeElement(wizardFireball, WIZARD_FIREBALL_COLORS, changeElementBackground, inputWizardFireball);
+  window.colorize.colorizeElement(wizardCoatElement, WIZARD_COAT_COLORS, fillElement, inputWizardCoatElement);
+  window.colorize.colorizeElement(wizardEyesElement, WIZARD_EYES_COLORS, fillElement, inputWizardEyesElement);
+  window.colorize.colorizeElement(wizardFireballElement, WIZARD_FIREBALL_COLORS, changeElementBackground, inputWizardFireballElement);
 })();
